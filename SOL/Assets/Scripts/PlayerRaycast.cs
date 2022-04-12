b7545bbc34e7f0af4fd6de333db1c5cd977647d8
stolenlegos <stolenlegos@gmail.com>
2022-04-12 12:59:15 -0400

    Quantum Shards

---
+++ SOL/Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+      //  Ray ray = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(transform.position, transform.forward * 100, Color.green);
+      //  RaycastHit hit;
+      //  Physics.Raycast(ray, out hit);
+
+        //if(hit.collider.tag == "MakeVisible") {
+        //  Debug.Log("Make Visible");
+        //}
+    }
+}
